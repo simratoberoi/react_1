1f4c5e10cac403eb88f350b3a050ca7e9e11fc8b
Simrat Oberoi <simratoberoi2006@gmail.com>
2025-09-22 22:22:34 +0530

    list updated

--- proj_2/src/App.jsx
+++ proj_2/src/App.jsx
@@ -5,10 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
-  let teams=["aston martin", "mercedes", "Red bull", "mclaren", "alpine", "stake sauber", "williams racing", "Ferrari", "Haas", "racing bulls"];
+  let teams=[/*"aston martin", "mercedes", "Red bull", "mclaren", "alpine", "stake sauber", "williams racing", "Ferrari", "Haas", "racing bulls"*/];
+  /*if (teams.length==0){
+    return <h3>No teams this season</h3>
+  }*/
   return (
     <>
       <h1>F1 Teams - 2025</h1>
+       {teams.length==0? <h3>No teams this season</h3>: null}  
       <ul class="list-group">
         {teams.map((item) => (
           <li class="list-group-item">{item}</li>
